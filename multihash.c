881217ffc697a77f625375bab310995ce4ca7335
jack <james.casebeer@protonmail.com>
2021-04-10 18:13:23 -0400

    even smaller hash tables

---
+++ multihash.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+
+struct multihash {
+    uint32_t   *key;
+    uint32_t   *val;
+    uint32_t unused; 
+    size_t      cap;
+};
+
+uint32_t hash32(uint32_t x)
+{
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+struct multihash multihash_init(uint32_t *key, uint32_t *val, size_t cap, uint32_t unused)
+{
+    struct multihash h = {0};
+    size_t i;
+    for(i = 0; i<cap; i++)
+        key[i] = unused;
+    h.key = key;
+    h.val = val;
+    h.unused = unused;
+    h.cap = cap;
+    return h;
+}
+
+void multihash_set(struct multihash *h, uint32_t key, uint32_t val)
+{
+    size_t slot = hash32(key) % (h->cap);
+    while(h->key[slot] != h->unused)
+        slot = (slot + 1) % (h->cap);
+    h->key[slot] = key;
+    h->val[slot] = val;
+}   
+
+size_t multihash_get(struct multihash *h, uint32_t key, uint32_t *out)
+{
+    size_t slot = hash32(key) % (h->cap);
+    size_t found = 0;
+    while(h->key[slot] != h->unused) {
+        if (h->key[slot] == key)
+            out[found++] = h->val[slot];
+        slot = (slot + 1) % (h->cap);
+    }
+    return found;
+}
+
+void multihash_get_bitset(struct multihash *h, uint32_t key, uint64_t *out)
+{
+    size_t slot = hash32(key) % (h->cap);
+    while(h->key[slot] != h->unused) {
+        if (h->key[slot] == key) {
+            uint64_t val = h->val[slot];
+            out[val>>6] = out[val>>6] | (1 << (val & 63));
+        }
+        slot = (slot + 1) % (h->cap);
+    }
+}
+
+enum {
+    MAX_AABB=256,
+    HASH_SIZE=MAX_AABB*4*2,
+    WORLD_WIDTH=128,
+    WORLD_HEIGHT=64,
+    GRID_SQUARE_WIDTH = 16,
+    GRID_SQUARE_HEIGHT =16 
+};
+
+typedef struct v2 {
+    int x; 
+    int y; 
+}v2;
+
+struct aabb {
+    v2 min;
+    v2 max;
+};
+
+int aabb_contains(struct aabb box, v2 point)
+{
+    return (point.x >= box.min.x && 
+            point.x <  box.max.x && 
+            point.y >= box.min.y &&
+            point.y <  box.max.y);
+}
+
+uint32_t grid_location(int x, int y)
+{
+    return (x | ((y) << 16));
+}
+
+struct aabb grid_span(struct aabb box)
+{
+    box.min.x = box.min.x / GRID_SQUARE_WIDTH;
+    box.min.y = box.min.y / GRID_SQUARE_HEIGHT;
+    box.max.x = box.max.x / GRID_SQUARE_WIDTH;
+    box.max.y = box.max.y / GRID_SQUARE_HEIGHT;
+    return box;
+}
+
+size_t multihash_get_span(struct multihash *h, struct aabb span, uint32_t *out)
+{
+    size_t num = 0;
+    // supports id's as high as 1024
+    uint64_t set[16] = {0};
+    for(int x = span.min.x; x<=span.max.x; x++)
+        for(int y = span.min.y; y<=span.max.y; y++) {
+            multihash_get_bitset(h, grid_location(x,y), set); 
+        }
+    for(int i = 0; i<16; i++) {
+        uint32_t bitset = set[i];
+        uint32_t mag = i<<6;
+        while(bitset != 0) {
+            out[num++] = mag + __builtin_ctzll(bitset); 
+            bitset &= (bitset - 1);
+        }
+    }
+    return num;
+}
+
+int main()
+{
+    uint32_t     keys[HASH_SIZE] = {0};
+    uint32_t     vals[HASH_SIZE] = {0};
+    struct aabb dudes[MAX_AABB]  = {0};
+
+    uint32_t bucket[HASH_SIZE] = {0};
+    struct multihash map = multihash_init(keys, vals, HASH_SIZE, (uint32_t)-1);
+
+    int i;
+    for(i = 0; i<MAX_AABB; i++) {
+        struct aabb dude = {0};          
+        dude.min.x = rand() & (WORLD_WIDTH  - 1);
+        dude.min.y = rand() & (WORLD_HEIGHT - 1);
+        dude.max.x = dude.min.x + 4;
+        dude.max.y = dude.min.y + 4;
+        dudes[i] = dude;
+        struct aabb span = grid_span(dude);
+        for(int x = span.min.x; x<=span.max.x; x++)
+            for(int y = span.min.y; y<=span.max.y; y++)
+                multihash_set(&map, grid_location(x,y), i);
+    }
+
+    int y, x;
+    for(y = 0; y<WORLD_HEIGHT; y++) {
+        for(x = 0; x<WORLD_WIDTH; x++){
+            int found_dude = 0;
+            size_t num_aabbs = multihash_get(&map, grid_location(x/GRID_SQUARE_WIDTH,y/GRID_SQUARE_HEIGHT), bucket);
+            for(i=0; i<num_aabbs; i++) {
+                struct aabb dude = dudes[bucket[i]]; 
+                if (aabb_contains(dude, (v2) {x,y})) {
+                    found_dude=1;
+                    break;
+                }
+            }
+            if (found_dude)
+                printf("O");
+            else
+                printf(" ");
+
+        }
+        printf("\n");
+    }
+//#if 0
+    for(i = 0; i<HASH_SIZE; i++) {
+        //printf("slot %8d, key: %8d, val: %8d\n", i, map.key[i], map.val[i]);
+        printf("%8d %8x\n", map.val[i], map.key[i]);
+    }
+//#endif
+printf("%d\n", sizeof(keys) + sizeof(vals));
+    return 0;
+}
---
+++ tiny_hash.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+struct hash {
+    uint32_t *key;
+    uint32_t *val;
+    uint32_t zero_val;
+    size_t cap;
+};
+
+struct str_hash {
+    const char **key;
+    const char **val;
+    size_t cap;
+};
+
+uint32_t hash32(uint32_t x)
+{
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+uint32_t hash_string(const char *s)
+{
+    uint32_t hash = 0x811c9dc5;
+    while(*s) {
+        hash = hash ^ (uint32_t)(*s);
+        hash = hash * 0x01000193;
+        s++;
+    }
+    return hash;
+}
+
+struct str_hash str_hash_init(const char **keys, const char **vals, size_t size)
+{
+    struct str_hash hash = {keys, vals, size}; 
+    return hash;
+}
+
+void str_hash_set(struct str_hash *h, const char *key, const char *val)
+{
+    size_t i = hash_string(key) & (h->cap - 1);
+    while(h->key[i] != NULL && strcmp(h->key[i], key) != 0)
+        i = (i+1) & (h->cap - 1);
+    h->key[i] = key;
+    h->val[i] = val;
+}
+
+const char *str_hash_get(struct str_hash *h, const char *key)
+{
+    size_t i = hash_string(key) & (h->cap - 1); 
+    while(h->key[i] != NULL && strcmp(h->key[i], key) != 0)
+        i = (i+1) & (h->cap - 1);
+    return h->val[i]; 
+}
+
+struct hash hash_init(uint32_t *keys, uint32_t *vals, size_t size)
+{
+    struct hash hash = {keys, vals, 0, size};
+    return hash;
+}
+
+void hash_set(struct hash *h, uint32_t key, uint32_t val)
+{
+    if (key == 0) {
+        h->zero_val = val;
+        return;
+    }
+    size_t i = hash32(key) & (h->cap - 1);
+    while(h->key[i] != 0 && h->key[i] != key)
+        i = (i+1) & (h->cap - 1);
+    h->key[i] = key; 
+    h->val[i] = val; 
+}
+
+uint32_t hash_get(struct hash *h, uint32_t key)
+{
+    if (key==0)
+        return h->zero_val;
+    size_t i = hash32(key) & (h->cap - 1);
+    while(h->key[i] != 0 && h->key[i] != key)
+        i = (i+1) & (h->cap - 1);
+    return h->key[i] == key ? h->val[i] : 0;
+}
+
+void print_spatial_hash(struct hash *bhash, int width, int height)
+{
+    for(int row = 0; row<height; row++) {
+        for(int col = 0; col<width; col++) {
+            uint32_t coord = (row<<16) | col;
+            if (hash_get(bhash, coord))
+                printf("O");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+void clear_screen()
+{ 
+    printf("\e[1;1H\e[2J");
+}
+
+void reset_cursor()
+{
+    printf("\e[1;1H");
+}
+
+
+int main()
+{
+    srand(time(NULL));
+    
+    uint32_t keys[32] = {0};
+    uint32_t vals[32] = {0};
+    struct hash hash = hash_init(keys, vals, 32);
+
+    for(uint32_t i = 0; i<16; i++) {
+        uint32_t val = i*i;
+        hash_set(&hash, i, val);
+        assert(hash_get(&hash, i) == val);
+    }
+
+    const char *str_keys[32] = {0};
+    const char *str_vals[32] = {0};
+    struct str_hash str_hash = str_hash_init(str_keys, str_vals, 32);
+
+    str_hash_set(&str_hash, "hello", "world");
+    str_hash_set(&str_hash, "hello darkness", "my old friend");
+    str_hash_set(&str_hash, "peanut butter", "jelly");
+    str_hash_set(&str_hash, "gold", "silver");
+    char hello_key[] = {'h','e','l','l','o', '\0'};
+    assert(strcmp(str_hash_get(&str_hash, hello_key), "world") == 0);
+    assert(strcmp(str_hash_get(&str_hash, "hello darkness"), "my old friend") == 0);
+    assert(strcmp(str_hash_get(&str_hash, "peanut butter"), "jelly") == 0);
+    assert(strcmp(str_hash_get(&str_hash, "gold"), "silver") == 0);
+    
+
+    uint32_t block_keys[1024] = {0};
+    uint32_t block_vals[1024] = {0};
+    struct hash bhash = hash_init(block_keys, block_vals, 1024);
+
+    const int blocks = 512;
+    const int width = 120;
+    const int height = 60;
+    int x = width/2;
+    int y = height/2;
+
+    struct coord {
+        int x,y;
+    };
+    
+    struct coord cardinals[4] = {
+        { 0, 1},
+        { 0,-1},
+        { 1, 0},
+        {-1, 0},
+    };
+    
+    clear_screen();
+    for(int i = 0; i<blocks; i++) {
+        uint32_t coord = (y<<16) | x;
+        if (hash_get(&bhash, coord))
+            i--;
+        hash_set(&bhash, coord, 1);
+        struct coord diff = cardinals[rand() % 4];
+        x += diff.x;
+        y += diff.y;
+        while (x<0 || y<0 || x>=width || y>=height) {
+           x = rand() % width; 
+           y = rand() % height; 
+        }
+        reset_cursor();
+        print_spatial_hash(&bhash, width, height);
+        usleep(1000);
+    }
+
+    return 0;
+}
